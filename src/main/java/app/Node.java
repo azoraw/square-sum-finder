c2efaa582e7e6658e789359dff349649914ebb6c
Albert Żóraw <albert.zoraw@xe05.ey.com>
2019-04-14 22:54:49 +0200

    git init

---
+++ src/main/java/app/Node.java
@@ -0,0 +1,15 @@
+package app;
+
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+public class Node {
+
+    Integer integer;
+    List<Integer> linkedNodes = new ArrayList<>();
+
+
+}
